06e7e59daa414fcc0b33c386b3d19e38553b2304
mahendra-gehlot <connect.mge@gmail.com>
2022-07-02 02:36:57 +0530

    v5

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 import streamlit as st
-import pandas as pd
-from transformers import pipeline, set_seed
+import torch
+from transformers import GPT2Tokenizer, GPT2Model
 
 st.title("Text Generation Model")
 
 # Loading Model
 with st.spinner('Working on it ...'):
-    generator = pipeline('text-generation', model='gpt2')
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2Model.from_pretrained('gpt2')
 st.success('Hurrah, Ready to go!!')
 
 
 # Processing Input
 with st.form("Model Input"):
     st.text_input("Input to Model", key="input")
-    slider_val = st.select_slider("Specify Response Length", options=[
-                                  10, 15, 20, 25, 30])
-    num_responses = st.selectbox("Number of responses:", (1, 2, 3, 4, 5))
     submitted = st.form_submit_button("Generate Text")
     if submitted:
-        generated_text = generator(st.session_state.input, max_length=int(
-            slider_val), num_return_sequences=num_responses)
-        container = st.container()
-        responses = list()
-        for response in generated_text:
-            responses.append(response["generated_text"])
-        df = pd.DataFrame({
-            'Responses': responses
-        })
-        df.index = df.index + 1
-        st.dataframe(df)
+        encoded_input = tokenizer(st.session_state.input, return_tensors='pt')
+        output = model(**encoded_input)
+        st.write(output)
